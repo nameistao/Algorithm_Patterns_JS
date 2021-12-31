4699324b477c9a9711ced087b69e95de36e4e34a
tao luo <taoluo@usc.edu>
2021-12-30 23:39:19 -0800

    add additional test case

--- slidingWindow/subarrayOfSizeK/index.test.js
+++ slidingWindow/subarrayOfSizeK/index.test.js
@@ -8,3 +8,7 @@ test("test 1", () => {
 test("test 2", () => {
   expect(func([2, 3, 4, 1, 5], 2)).toBe(7);
 });
+
+test("test 3", () => {
+  expect(func([1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 3, 2, 1], 5)).toBe(29);
+});
