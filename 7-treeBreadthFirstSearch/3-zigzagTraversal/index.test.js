5ab8caf4e723b4f9817ba41066bdd4187a01503c
tao luo <taoluo@usc.edu>
2022-01-16 13:12:26 -0800

    add additional test case

--- 7-treeBreadthFirstSearch/3-zigzagTraversal/index.test.js
+++ 7-treeBreadthFirstSearch/3-zigzagTraversal/index.test.js
@@ -19,3 +19,14 @@ test("test 1", () => {
   root.right.left.right = new TreeNode(17);
   expect(func(root)).toEqual([12, 1, 7, 9, 10, 5, 17, 20]);
 });
+
+test("test 1", () => {
+  const root = new TreeNode(1);
+  root.left = new TreeNode(2);
+  root.right = new TreeNode(3);
+  root.left.left = new TreeNode(4);
+  root.left.right = new TreeNode(5);
+  root.right.left = new TreeNode(6);
+  root.right.right = new TreeNode(7);
+  expect(func(root)).toEqual([1, 3, 2, 4, 5, 6, 7]);
+});
